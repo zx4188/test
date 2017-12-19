07f99192af34f30061772995f8c9cf1aab53e228
zx4188@163.com <zx4188@163.com>
2017-12-19 21:33:36 +0800

    12-19

---
+++ src/main/java/it18zhang/SparkStreaming/App.java
@@ -0,0 +1,13 @@
+package it18zhang.SparkStreaming;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
